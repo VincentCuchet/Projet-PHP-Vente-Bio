f10ae9ae6ae615c745d39b8c15e42ceb9ae3e979
VincentCuchet <vincent.cuchet@gmail.com>
2016-01-13 14:35:09 +0100

    Revert "Change titre"
    This reverts commit 55e9735414c7d9b0abf96e54902bfc67f006ea4f.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>         
 	<meta http-equiv="Content-type" content= "text/html;charset=UTF-8" /> 
 	<link href="style.css" rel="stylesheet" type="text/css">        
-	<title>Porjet Acsi Oracle</title>      
+	<title>Les produits bios du pays</title>      
 	
 	<script src="js/jquery.js" type="text/javascript"></script><!-- Insertion de la bibliotheque jQuery -->
 	<script type="text/javascript" src="js/localscroll/jquery.localscroll.js"></script>
